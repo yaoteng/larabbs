f3b22364a57fc0622328914f59dbba2e3883c3c5
yaoteng <pangzi.zi@163.com>
2019-05-04 17:02:32 +0800

    翻译队列化

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -22,7 +22,7 @@ class TopicsController extends Controller
 		return view('topics.index', compact('topics'));
 	}
 
-    public function show(Topic $topic)
+    public function show(Request $request,Topic $topic)
     {   // URL 矫正
         if ( ! empty($topic->slug) && $topic->slug != $request->slug) {
             return redirect($topic->link(), 301);
