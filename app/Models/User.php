d05832d4ae4327c777f3611bc19a99fb76ee87b6
yaoteng <pangzi.zi@163.com>
2019-05-04 17:35:35 +0800

    回复数据

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->id == $model->user_id;
     }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
