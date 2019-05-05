5b4f5a7123ccde42a1d90463be9b3768c245c6eb
yaoteng <pangzi.zi@163.com>
2019-05-05 10:51:41 +0800

    消息通知列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,20 +14,6 @@ class User extends Authenticatable implements MustVerifyEmailContract
         notify as protected laravelNotify;
     }
 
-    public function notify($instance)
-    {
-        // 如果要通知的人是当前用户，就不必通知了！
-        if ($this->id == Auth::id()) {
-            return;
-        }
-
-        // 只有数据库类型通知才需提醒，直接发送 Email 或者其他的都 Pass
-        if (method_exists($instance, 'toDatabase')) {
-            $this->increment('notification_count');
-        }
-
-        $this->laravelNotify($instance);
-    }
     /**
      * The attributes that are mass assignable.
      *
@@ -68,5 +54,26 @@ class User extends Authenticatable implements MustVerifyEmailContract
     public function replies()
     {
         return $this->hasMany(Reply::class);
+    }
+    public function notify($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了！
+        if ($this->id == Auth::id()) {
+            return;
+        }
+
+        // 只有数据库类型通知才需提醒，直接发送 Email 或者其他的都 Pass
+        if (method_exists($instance, 'toDatabase')) {
+            $this->increment('notification_count');
+        }
+
+        $this->laravelNotify($instance);
+    }
+    
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
     }
 }
