4a975b3d071d0f0def18ee1668466bb6913dbfa2
yaoteng <pangzi.zi@163.com>
2019-05-05 12:30:03 +0800

    集成 laravel-permission 扩展包

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,14 @@ use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Auth;
+use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable implements MustVerifyEmailContract
 {
     use MustVerifyEmailTrait;
     use Notifiable {
         notify as protected laravelNotify;
     }
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
