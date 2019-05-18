ea014838963ccf1106e3d31bd8f4c70e7a2ec77b
yaoteng <pangzi.zi@163.com>
2019-05-18 22:03:35 +0800

    replies index

--- routes/api.php
+++ routes/api.php
@@ -80,6 +80,15 @@ $api->version('v1', [
             // 删除回复
             $api->delete('topics/{topic}/replies/{reply}', 'RepliesController@destroy')
                 ->name('api.topics.replies.destroy');
+            // 某个用户发布的话题
+            $api->get('users/{user}/topics', 'TopicsController@userIndex')
+                ->name('api.users.topics.index');
+            // 话题回复列表
+            $api->get('topics/{topic}/replies', 'RepliesController@index')
+                ->name('api.topics.replies.index');
+            // 某个用户的回复列表
+            $api->get('users/{user}/replies', 'RepliesController@userIndex')
+                ->name('api.users.replies.index');
         });
     });
 });
