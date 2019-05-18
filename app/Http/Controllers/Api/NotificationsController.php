5a4a03f6931bf77a4b6f1ad016d33b28874bcbd4
yaoteng <pangzi.zi@163.com>
2019-05-19 04:34:04 +0800

    notifications read

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -22,4 +22,11 @@ class NotificationsController extends Controller
             'unread_count' => $this->user()->notification_count,
         ]);
     }
+
+    public function read()
+    {
+        $this->user()->markAsRead();
+
+        return $this->response->noContent();
+    }
 }
